453ffbf549ca1ade0bbb14005fb2d9aae67fd84d
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2026-01-01 18:57:01 +0100

    refactor Home component and styles: simplify structure, update carousel image width, and clean up commented code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,21 +5,9 @@ import { Carousel } from "./Carousel.jsx";
 
 export function Home() {
     return (
-        <>
+        <div className="container">
             <div className="decor"></div>
-            {/* <Carousel className="carousel" /> */}
-        </>
-
-        // <div className="container">
-        //     <div className="left-section">
-        //         <div className="decor"></div>
-        //         <Carousel className="carousel" />
-        //     </div>
-        //     {/* <div className="right-section"> */}
-        //         {/* <div className="header-container"> */}
-        //             {/* <Header /> */}
-        //         {/* </div> */}
-        //     {/* </div> */}
-        // </div>
+            <Carousel className="carousel" />
+        </div>
     );
 }
