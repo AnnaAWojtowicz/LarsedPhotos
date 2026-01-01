0ba91b153bbc693576024a578830e10fec0c620f
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2026-01-01 09:30:49 +0100

    refactor layout: simplify container structure and remove commented code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../styles/home.css";
 import "../App.css";
 import { Carousel } from "./Carousel.jsx";
@@ -6,18 +5,16 @@ import { Header } from "./Header.jsx";
 
 export function Home() {
     return (
-        <>
-            <div className="container">
-                <div className="left-section">
-                    <div className="decor"></div>
-                    <Carousel className="carousel" />
-                </div>
-                <div className="right-section">
-                    <div className="header-container">
-                        <Header />
-                    </div>
+        <div className="container">
+            <div className="left-section">
+                <div className="decor"></div>
+                <Carousel className="carousel" />
+            </div>
+            <div className="right-section">
+                <div className="header-container">
+                    <Header />
                 </div>
             </div>
-        </>
+        </div>
     );
 }
