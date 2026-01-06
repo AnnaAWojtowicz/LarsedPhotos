b2418d101723af31f47c9ce88344f4119054d431
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2026-01-06 20:23:39 +0100

    refactor Home and LazyLoad components: update layout and styles for improved image loading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,14 @@ import "../App.css";
 import { Carousel } from "./Carousel.jsx";
 // import { Header } from "./Header.jsx";
 import { Menu } from "./Menu.jsx";
+import { LazyLoad } from "./LazyLoad.jsx";
 
 export function Home() {
     return (
         <div className="container">
             <div className="decor"></div>
-            <Carousel />
+            {/* <Carousel /> */}
+            <LazyLoad />
             <Menu />
         </div>
     );
--- src/components/LazyLoad.jsx
+++ src/components/LazyLoad.jsx
@@ -3,9 +3,11 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/black-and-white.css";
 import { getPhotos } from '../api/homeApi.js';
 import { svgPlaceholder } from '../constants/placeholders.js';
+import '../styles/lazyload.css'
 
 export function LazyLoad() {
 
+    const width = 600;
     const [isLoading, setIsLoading] = useState(false);
     const [allPhotos, setAllPhotos] = useState([]);
 
@@ -25,20 +27,22 @@ export function LazyLoad() {
     }, []);
 
 
-    return(
-        <div className="carousel">
-            {allPhotos.map((photo, index) =>(
-                <LazyLoadImage 
-                    key={photo.id} 
-                    src={photo.url800}
-                    width={photo.dimension800.width} 
-                    height={photo.dimension800.height}
-                    alt={photo.title || `Photo ${index}`}
-                    threshold = {1}
-                    effect='black-and-white'
-                    placeholderSrc={svgPlaceholder}
-                />
-            ))}
+    return (
+        <div className='lazy-column'>
+            {
+                allPhotos.map((photo, index) => (
+                    <LazyLoadImage
+                        key={photo.id}
+                        src={photo.url800}
+                        width={width}
+                        height={Math.round((width / photo.dimension800.width) * photo.dimension800.height)}
+                        alt={photo.title || `Photo ${index}`}
+                        threshold={1}
+                        effect='black-and-white'
+                        placeholderSrc={svgPlaceholder}
+                    />
+                ))
+            }
         </div>
     );
 }
