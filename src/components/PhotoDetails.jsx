7114bc56be5635f07a7c87b03c5aee3758ed7f64
Anna Wojtowicz <anna.agnieszka.wojtowicz@gmail.com>
2026-02-07 00:20:31 +0100

    fix(PhotoDetails): integrate map display for photo location and enhance EXIF data layout

--- src/components/PhotoDetails.jsx
+++ src/components/PhotoDetails.jsx
@@ -2,6 +2,7 @@ import { useParams, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { usePhotos } from '../context/PhotosContext.jsx';
 import { getSpecificImgDetails } from '../api/getSpecificImgDetails.js';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import "../styles/photoDetails.css";
 
 export function PhotoDetails() {
@@ -50,6 +51,8 @@ export function PhotoDetails() {
         return defaultFocalLength ?? standardFocalLenght;
     }
 
+    const position = [51.505, -0.09]
+
     return (
         <>
             {loading && <p>Loading...</p>}
@@ -61,30 +64,59 @@ export function PhotoDetails() {
                     </div>
                 }
                 {photoExifData &&
-                    <div className='exif'>
-                        <div className='exif-symbol-group'>
-                            <div className='material-symbols-outlined exif-symbols'>{getCamerasymbol()}</div>
-                            <div className='exif-double'>
-                                <div className='exif-data'>{photoExifData.results[0].camera.fullName}</div>
-                                {photoExifData.results[0].camera.brand !== "DJI" && //only show if not a drone shot
-                                    <div className='exif-data'>{photoExifData.results[0].lens.lens}</div>
-                                }
+                    <>
+                        <div className='exif'>
+                            <div className='exif-symbol-group'>
+                                <div className='material-symbols-outlined exif-symbols'>{getCamerasymbol()}</div>
+                                <div className='exif-double'>
+                                    <div className='exif-data'>{photoExifData.results[0].camera.fullName}</div>
+                                    {photoExifData.results[0].camera.brand !== "DJI" && //only show if not a drone shot
+                                        <div className='exif-data'>{photoExifData.results[0].lens.lens}</div>
+                                    }
+                                </div>
+                            </div>
+                            <div className='exif-symbol-group'>
+                                <div className='material-symbols-outlined exif-symbols'>shutter_speed</div>
+                                <div className='exif-data'>{photoExifData.results[0].exposure.exposureTime}s</div>
+                            </div>
+                            <div className='exif-symbol-group'>
+                                <div className='material-symbols-outlined exif-symbols'>camera</div>
+                                <div className='exif-data'>{photoExifData.results[0].exposure.aperture}s</div>
+                            </div>
+                            <div className='exif-symbol-group'>
+                                <div className='material-symbols-outlined exif-symbols'>straighten</div>
+                                <div className='exif-data'>{getAvailableFocalLength()}</div>
                             </div>
                         </div>
-                        <div className='exif-symbol-group'>
-                            <div className='material-symbols-outlined exif-symbols'>shutter_speed</div>
-                            <div className='exif-data'>{photoExifData.results[0].exposure.exposureTime}s</div>
+                        <div className='photo-map'>
+                            <p>Location</p>
+                            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                                <TileLayer
+                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <Marker position={position}>
+                                    <Popup>
+                                        A pretty CSS3 popup. <br /> Easily customizable.
+                                    </Popup>
+                                </Marker>
+                            </MapContainer>
                         </div>
-                        <div className='exif-symbol-group'>
-                            <div className='material-symbols-outlined exif-symbols'>camera</div>
-                            <div className='exif-data'>{photoExifData.results[0].exposure.aperture}s</div>
-                        </div>
-                        <div className='exif-symbol-group'>
-                            <div className='material-symbols-outlined exif-symbols'>straighten</div>
-                            <div className='exif-data'>{getAvailableFocalLength()}</div>
-                        </div>
-                    </div>
+                    </>
                 }
+                <div className='photo-map'>
+                    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={position}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
             </div>
             {!loading && !myPhoto && <p>Photo not found</p>}
         </>
